e4bbcfb9ac306aa44ceb69e33dbaa45600275f74
lleps <leandro.barbero122@gmail.com>
2019-05-29 20:24:53 -0300

    Extract view code to MainView

--- src/main/java/com/lleps/tradexchange/App.kt
+++ src/main/java/com/lleps/tradexchange/App.kt
@@ -1,16 +1,10 @@
 package com.lleps.tradexchange
 
 import com.lleps.tradexchange.view.FullChart
+import com.lleps.tradexchange.view.MainView
 import javafx.application.Application
-import javafx.application.Platform
-import javafx.collections.FXCollections
 import javafx.scene.Scene
-import javafx.scene.control.*
-import javafx.scene.control.cell.PropertyValueFactory
 import javafx.scene.image.Image
-import javafx.scene.layout.BorderPane
-import javafx.scene.layout.VBox
-import javafx.scene.paint.Color
 import javafx.stage.Stage
 import org.apache.log4j.AppenderSkeleton
 import org.apache.log4j.Logger
@@ -23,23 +17,57 @@ import java.time.ZonedDateTime
 import kotlin.concurrent.thread
 
 class App : Application() {
-    private lateinit var chart: FullChart
+    private lateinit var view: MainView
     private lateinit var exchange: Exchange
-    private lateinit var state: TradePersistentState
-
-    private enum class TradeType { BUY, SELL }
-
-    private data class TradeEvent(
-        val id: Int,
-        val type: TradeType,
-        val coins: Double,
-        val price: Double,
-        val epoch: Long
-    )
-
-    private data class TradePersistentState(val events: List<TradeEvent>, val unSoldCoins: List<TradeEvent>)
 
     private enum class Mode { BACKTEST, LIVE }
+
+    private fun onExecute(input: Map<String, String>) {
+        view.toggleExecute(false)
+        LOGGER.info("Input: $input")
+        val trades = mutableListOf<MainView.TradeEntry>()
+        runStrategy(input,
+            mode = Mode.BACKTEST,
+            onTrade = { buy, sell, amount ->
+                // TODO: why trades are recorded here but chart data is recorded at
+                //  the same time the strategy is executed?
+                trades.add(MainView.TradeEntry(1, buy, sell, amount))
+            },
+            onFinish = {
+                view.toggleExecute(true)
+                view.setTrades(trades)
+            })
+    }
+
+    override fun start(stage: Stage) {
+        view = MainView()
+        view.onExecute { onExecute(it) }
+        stage.scene = Scene(view.initJavaFxContent())
+        stage.icons.add(Image("money-icon.png"))
+        stage.title = "Tradexchange"
+        stage.show()
+
+        // Set default strategy input
+        val defaultInput = mutableMapOf(
+            "pair" to "USDT_ETH",
+            "period" to "300",
+            "days" to "7",
+            "initialMoney" to "1000.0",
+            "initialCoins" to "0.0",
+            "plotChart" to "3"
+        )
+        Strategy.requiredInput.forEach { key, value -> defaultInput[key] = value.toString() }
+        view.setInput(defaultInput)
+
+        // Attach logger to the view
+        Logger.getRootLogger().addAppender(object : AppenderSkeleton() {
+            override fun append(event: LoggingEvent) {
+                view.log(event.message.toString())
+            }
+            override fun close() {}
+            override fun requiresLayout() = false
+        })
+    }
 
     private fun runStrategy(
         input: Map<String, String>,
@@ -57,11 +85,8 @@ class App : Application() {
                     Mode.BACKTEST -> {
                         // Set up
                         LOGGER.info("Starting backtesting $days-day for $pair... (period: ${period/60} min)")
-
                         val initialMoney = 1000.0
                         val initialCoins = 0.0
-
-                        // set up view data structures
                         val chartOperations = mutableListOf<FullChart.Operation>()
                         val candles = mutableListOf<FullChart.Candle>()
                         val priceIndicators = mutableMapOf<String, MutableMap<Long, Double>>()
@@ -83,6 +108,7 @@ class App : Application() {
                             }
                         }
 
+                        // Fetch data
                         exchange = PoloniexBacktestExchange(
                             pair = pair,
                             period = period,
@@ -90,15 +116,13 @@ class App : Application() {
                             warmUpTicks = 100,
                             initialMoney = initialMoney,
                             initialCoins = initialCoins)
-
                         val allTicks = mutableListOf(*exchange.warmUpHistory.toTypedArray())
-
                         while (true) {
                             val tick = exchange.fetchTick() ?: break
                             allTicks += tick
                         }
 
-                        // Test
+                        // Execute strategy
                         val timeSeries = BaseTimeSeries(allTicks)
                         val strategy = Strategy(
                             series = timeSeries,
@@ -108,7 +132,6 @@ class App : Application() {
                             exchange = exchange,
                             input = input
                         )
-
                         for (i in exchange.warmUpHistory.size..timeSeries.endIndex) {
                             val tick = timeSeries.getTick(i)
                             val epoch = tick.beginTime.toEpochSecond()
@@ -134,10 +157,9 @@ class App : Application() {
                             }
                         }
 
-                        // Resume
+                        // Print resume
                         val firstPrice = ClosePriceIndicator(timeSeries)[exchange.warmUpHistory.size]
                         val latestPrice = ClosePriceIndicator(timeSeries)[timeSeries.endIndex]
-
                         LOGGER.info(" ______________________________________________________ ")
                         LOGGER.info("                  RESULTS                               ")
                         LOGGER.info("Initial balance        %.03f'c $%.03f"
@@ -151,15 +173,14 @@ class App : Application() {
                         LOGGER.info("Trades: ${strategy.tradeCount}")
                         LOGGER.info(" ______________________________________________________ ")
 
-                        // Draw chart
-                        chart.priceData = if (plotChart >= 1) candles else emptyList()
-                        chart.operations = if (plotChart >= 1) chartOperations else emptyList()
-                        chart.priceIndicators = if (plotChart >= 1) priceIndicators else emptyMap()
-                        chart.extraIndicators = if (plotChart >= 1) extraIndicators else emptyMap()
-                        LOGGER.info("Done")
-                        Platform.runLater {
-                            chart.fill()
-                        }
+                        // Update view
+                        view.setChart(
+                            candles = if (plotChart >= 1) candles else emptyList(),
+                            operations = if (plotChart >= 1) chartOperations else emptyList(),
+                            priceIndicators = if (plotChart >= 1) priceIndicators else emptyMap(),
+                            extraIndicators = if (plotChart >= 1) extraIndicators else emptyMap()
+                        )
+                        LOGGER.info("Done!")
                         onFinish()
                     }
                     Mode.LIVE -> TODO("Implement live mode")
@@ -171,127 +192,11 @@ class App : Application() {
         }
     }
 
-    class TradeTableEntry(val buyPrice: Double, val sellPrice: Double, val amount: Double, val profit: Double)
-
-    private fun createTradesTable(trades: List<TradeTableEntry>): TableView<TradeTableEntry> {
-        val items = FXCollections.observableArrayList<TradeTableEntry>(*trades.toTypedArray())
-        val table = TableView(items)
-        // this cell factory format objects as prices
-        val cellFactory = {
-            object : TableCell<TradeTableEntry, Any>() {
-                override fun updateItem(item: Any?, empty: Boolean) {
-                    super.updateItem(item, empty)
-                    if (item == null || empty) {
-                        text = null
-                        style = ""
-                    } else {
-                        val color = if (item.toString().contains("-")) Color.ORANGERED else Color.GREEN
-                        text = "\$$item"
-                        textFill = color
-                    }
-                }
-            }
-        }
-        table.columns.add(TableColumn<TradeTableEntry, Any>("buy").apply {
-            cellValueFactory = PropertyValueFactory("buyPrice")
-            setCellFactory { cellFactory() }
-        })
-        table.columns.add(TableColumn<TradeTableEntry, Any>("sell")
-            .apply {
-                cellValueFactory = PropertyValueFactory("sellPrice")
-                setCellFactory { cellFactory() }
-            })
-        table.columns.add(TableColumn<TradeTableEntry, Any>("amount")
-            .apply {
-                cellValueFactory = PropertyValueFactory("amount")
-            })
-        table.columns.add(TableColumn<TradeTableEntry, Any>("profit")
-            .apply {
-                cellValueFactory = PropertyValueFactory("profit")
-                setCellFactory { cellFactory() }
-            })
-        return table
-    }
-
-    override fun start(stage: Stage) {
-        // Init scene, chart and main pane
-        chart = FullChart()
-        val controlPane = VBox()
-        stage.scene = Scene(BorderPane(chart, null, controlPane, null, null))
-        stage.icons.add(Image("money-icon.png"))
-        stage.title = "Tradexchange"
-        stage.show()
-        val outputPane = TextArea()
-
-        val tradeTableItems = mutableListOf<TradeTableEntry>()
-        val tradeTableContainer = BorderPane()
-        val tabOutput = Tab("Output", outputPane)
-        val tabTrades = Tab("Trades", tradeTableContainer)
-        val tabPane = TabPane(tabOutput, tabTrades)
-        tabPane.tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
-
-        // Input pane start
-        val defaultInput = mutableMapOf(
-            "pair" to "USDT_ETH",
-            "period" to "300",
-            "days" to "7",
-            "initialMoney" to "1000.0",
-            "initialCoins" to "0.0",
-            "plotChart" to "3"
-        )
-        Strategy.requiredInput.forEach { key, value -> defaultInput[key] = value.toString() }
-        val inputPane = VBox(5.0)
-        val inputUIElements = mutableListOf<Pair<Label, TextField>>()
-        for ((itemName, itemDefaultValue) in defaultInput) {
-            val label = Label(itemName)
-            val field = TextField(itemDefaultValue)
-            inputUIElements += Pair(label, field)
-            inputPane.children.add(BorderPane(null, null, field, null, label))
-        }
-        inputPane.children.add(Button("Ejecutar").apply {
-            setOnAction {
-                this.isDisable = true
-                // read input from ui
-                val map = mutableMapOf<String, String>()
-                for ((label, field) in inputUIElements) map[label.text] = field.text
-                outputPane.text = ""
-                LOGGER.info("Input: $map")
-                runStrategy(map, Mode.BACKTEST,
-                    onTrade = { buy, sell, amount ->
-                        tradeTableItems.add(TradeTableEntry(buy, sell, amount, (sell-buy)*amount))
-                    },
-                    onFinish = {
-                        Platform.runLater {
-                            this@apply.isDisable = false
-                            tradeTableContainer.center = createTradesTable(tradeTableItems)
-                            tradeTableItems.clear()
-                        }
-                    })
-            }
-        })
-        // ok. ejecutar deberia ejecutar la estrategia...
-        controlPane.children.add(inputPane)
-
-        // Log start
-        Logger.getRootLogger().addAppender(object : AppenderSkeleton() {
-            override fun append(event: LoggingEvent) {
-                Platform.runLater { outputPane.text += "${event.message}\n" }
-            }
-            override fun close() {}
-            override fun requiresLayout() = false
-        })
-        outputPane.prefWidth = 400.0
-        controlPane.children.add(tabPane)
-    }
-
     companion object {
         private val LOGGER = LoggerFactory.getLogger(App::class.java)
 
-        private lateinit var args: Array<String>
-
         @JvmStatic
         fun main(args: Array<String>) {
-            Companion.args = args
             launch(App::class.java)
         }
     }
--- src/main/java/com/lleps/tradexchange/view/MainView.kt
+++ src/main/java/com/lleps/tradexchange/view/MainView.kt
@@ -1,6 +1,14 @@
 package com.lleps.tradexchange.view
 
+import javafx.application.Platform
+import javafx.beans.property.SimpleObjectProperty
+import javafx.collections.FXCollections
 import javafx.scene.Parent
+import javafx.scene.control.*
+import javafx.scene.control.cell.PropertyValueFactory
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
 
 /**
  * Main client view. Contains controls to backtest, livetrade, etc.
@@ -9,26 +17,75 @@ import javafx.scene.Parent
  * through setters passing bulk data.
  */
 class MainView {
-    class TradeEntry(val id: Int, val buy: Double, val sell: Double)
+    class TradeEntry(val id: Int, val buy: Double, val sell: Double, val amount: Double)
+
+    private lateinit var chart: FullChart
+    private lateinit var outputPane: TextArea
+    private lateinit var executeButton: Button
+    private lateinit var tradeTableContainer: BorderPane
+    private lateinit var inputPane: VBox
+    private val inputUIElements = mutableListOf<Pair<Label, TextField>>()
+
+    private var onExecute: (Map<String, String>) -> Unit = {}
 
     fun initJavaFxContent(): Parent {
-        TODO()
+        // Main components
+        chart = FullChart()
+        val controlPane = VBox()
+        val mainPane = BorderPane(chart, null, controlPane, null, null)
+        outputPane = TextArea()
+
+        // Input
+        inputPane = VBox(5.0)
+        executeButton = Button("Execute").apply {
+            setOnAction { onExecute(readInput()) }
+        }
+
+        // Tabs
+        tradeTableContainer = BorderPane()
+        val tabOutput = Tab("Output", outputPane)
+        val tabTrades = Tab("Trades", tradeTableContainer)
+        val tabPane = TabPane(tabOutput, tabTrades)
+        tabPane.tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
+
+        outputPane.prefWidth = 300.0
+        controlPane.children.add(inputPane)
+        controlPane.children.add(executeButton)
+        controlPane.children.add(tabPane)
+        return mainPane
     }
 
-    fun onExecute(input: (Map<String, String>) -> Unit) {
+    private fun readInput(): Map<String, String>{
+        val result = mutableMapOf<String, String>()
+        for ((label, field) in inputUIElements) result[label.text] = field.text
+        return result
+    }
 
+    fun onExecute(callback: (Map<String, String>) -> Unit) {
+        onExecute = callback
     }
 
     fun toggleExecute(toggle: Boolean) {
-
+        Platform.runLater {
+            executeButton.isDisable = !toggle
+        }
     }
 
     fun setInput(input: Map<String, Any>) {
-
+        Platform.runLater {
+            inputPane.children.clear()
+            inputUIElements.clear()
+            for ((itemName, itemDefaultValue) in input) {
+                val label = Label(itemName)
+                val field = TextField(itemDefaultValue.toString())
+                inputUIElements += Pair(label, field)
+                inputPane.children.add(BorderPane(null, null, field, null, label))
+            }
+        }
     }
 
     fun log(message: String) {
-
+        Platform.runLater { outputPane.text += "$message\n" }
     }
 
     fun setChart(
@@ -36,10 +93,70 @@ class MainView {
         operations: List<FullChart.Operation>,
         priceIndicators: Map<String, Map<Long, Double>>,
         extraIndicators: Map<String, Map<String, Map<Long, Double>>>) {
-
+        chart.priceData = candles
+        chart.operations = operations
+        chart.priceIndicators = priceIndicators
+        chart.extraIndicators = extraIndicators
+        Platform.runLater { chart.fill() }
     }
 
     fun setTrades(trades: List<TradeEntry>) {
+        Platform.runLater { tradeTableContainer.center = createTradesTable(trades) }
+    }
 
+    private fun createTradesTable(trades: List<TradeEntry>): TableView<TradeEntry> {
+        val items = FXCollections.observableArrayList<TradeEntry>(*trades.toTypedArray())
+        val table = TableView(items)
+        // this cell factory format objects as prices if isMoney, otherwise as raw decimals but with fewer digits
+        val doubleFormattingCellFactory = { isMoney: Boolean ->
+            object : TableCell<TradeEntry, Any>() {
+                override fun updateItem(item: Any?, empty: Boolean) {
+                    super.updateItem(item, empty)
+                    if (item == null || empty) {
+                        text = null
+                        style = ""
+                    } else {
+                        val formatted = if (isMoney) "$%.04f" else "%.4f"
+                        text = formatted.format(item)
+                    }
+                }
+            }
+        }
+        table.columns.add(TableColumn<TradeEntry, Any>("#").apply {
+            cellValueFactory = PropertyValueFactory("id")
+        })
+        table.columns.add(TableColumn<TradeEntry, Any>("buy").apply {
+            cellValueFactory = PropertyValueFactory("buy")
+            setCellFactory { doubleFormattingCellFactory(true) }
+        })
+        table.columns.add(TableColumn<TradeEntry, Any>("sell")
+            .apply {
+                cellValueFactory = PropertyValueFactory("sell")
+                setCellFactory { doubleFormattingCellFactory(true) }
+            })
+        table.columns.add(TableColumn<TradeEntry, Any>("amount")
+            .apply {
+                cellValueFactory = PropertyValueFactory("amount")
+                setCellFactory { doubleFormattingCellFactory(false/*not money*/) }
+            })
+        table.columns.add(TableColumn<TradeEntry, TradeEntry>("profit")
+            .apply {
+                setCellValueFactory { cellData -> SimpleObjectProperty(cellData.value) }
+                setCellFactory { object : TableCell<TradeEntry, TradeEntry>() {
+                    override fun updateItem(item: TradeEntry?, empty: Boolean) {
+                        super.updateItem(item, empty)
+                        if (item == null || empty) {
+                            text = null
+                            style = ""
+                        } else {
+                            val profit = (item.sell-item.buy) * item.amount
+                            val color = if (profit <= 0.0) Color.ORANGERED else Color.GREEN
+                            text = "$%.3f".format(profit)
+                            textFill = color
+                        }
+                    }
+                } }
+            })
+        return table
     }
 }
