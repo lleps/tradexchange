3b8b2d2a73f3d259f27b5ecd709e103f5d73db1b
lleps <leandro.barbero122@gmail.com>
2019-06-06 00:07:43 -0300

    Add test algorithm vs random

--- src/main/java/com/lleps/tradexchange/strategy/CloseStrategy.kt
+++ src/main/java/com/lleps/tradexchange/strategy/CloseStrategy.kt
@@ -22,6 +22,14 @@ import kotlin.random.Random
 
 class CloseStrategy(val cfg: Config, val timeSeries: TimeSeries, val buyTick: Int, val buyPrice: Double) {
 
+    companion object {
+
+        private lateinit var emaLong: EMAIndicator
+        private lateinit var close: ClosePriceIndicator
+
+        private var inited = false
+    }
+
     class Config(
         var topBarrierInitial: Double = 2.0,
         var bottomBarrierInitial: Double = 2.0,
@@ -29,18 +37,25 @@ class CloseStrategy(val cfg: Config, val timeSeries: TimeSeries, val buyTick: In
         var timeWeightTop: Double = 2.0,
         var priceWeightBottom: Double = 2.0,
         var timeWeightBottom: Double = 2.0,
-        val longEmaPeriod: Int = 20
+        val longEmaPeriod: Int = 20,
+        var times: Int = 1
     )
 
-    private val close = ClosePriceIndicator(timeSeries)
-    private val emaLong = EMAIndicator(close, cfg.longEmaPeriod) // To detect market trend change
+    init {
+        if (!inited) {
+            close = ClosePriceIndicator(timeSeries)
+            emaLong = EMAIndicator(close, cfg.longEmaPeriod) // To detect market trend change
+            inited = true
+        }
+    }
+
     private var topBarrier: Double = buyPrice + pctToPrice(cfg.topBarrierInitial)
     private var bottomBarrier: Double = buyPrice - pctToPrice(cfg.bottomBarrierInitial)
     private var topBarrierCrossed = false
     private var timePassed: Int = 0
 
     /** Process the tick. Returns true if should close, false otherwise. */
-    fun doTick(i: Int, chart: Strategy.ChartWriter): Boolean {
+    fun doTick(i: Int, chart: Strategy.ChartWriter?): Boolean {
         val price = close[i]
         val epoch = timeSeries.getBar(i).endTime.toEpochSecond()
         val timePassed = (this.timePassed++).toDouble()
@@ -48,9 +63,11 @@ class CloseStrategy(val cfg: Config, val timeSeries: TimeSeries, val buyTick: In
         topBarrier += priceIncreasePct*cfg.priceWeightTop - timePassed*cfg.timeWeightTop
         bottomBarrier += priceIncreasePct.coerceAtLeast(0.0)*cfg.priceWeightBottom + timePassed*cfg.timeWeightBottom
 
-        chart.priceIndicator("ema", epoch, emaLong[i])
-        chart.priceIndicator("topBarrier", epoch, topBarrier)
-        chart.priceIndicator("bottomBarrier", epoch, bottomBarrier)
+        if (chart != null) {
+            chart?.priceIndicator("ema", epoch, emaLong[i])
+            chart?.priceIndicator("topBarrier", epoch, topBarrier)
+            chart?.priceIndicator("bottomBarrier", epoch, bottomBarrier)
+        }
 
         if (!topBarrierCrossed && price > topBarrier) {
             topBarrierCrossed = true
@@ -73,17 +90,19 @@ class CloseStrategy(val cfg: Config, val timeSeries: TimeSeries, val buyTick: In
         private var lastTickEnd = 0
         private lateinit var chart: FullChart
         private var profitSum: Double = 0.0
+        private var randomSum: Double = 0.0
         private lateinit var profitLabel: Label
 
         private fun execute(cfg: Config, retry: Boolean = false) {
             if (!retry) {
-                lastTickInitial = Random.nextInt(series.barCount)
+                lastTickInitial = Random.nextInt(series.barCount - 200)
                 lastTickEnd = lastTickInitial + 200
             }
             val initialTick = lastTickInitial + 50
             val initialPrice = series.getBar(initialTick).closePrice.doubleValue()
             val closeStrategy = CloseStrategy(cfg, series, initialTick, initialPrice)
-            val chartWriter = ChartWriterImpl()
+            val multipleTimes = cfg.times > 1
+            val chartWriter = if (!multipleTimes) ChartWriterImpl() else null
             val operations = mutableListOf<Operation>()
             var maxPrice = 0.0
             var minPrice = 999999.0
@@ -102,46 +121,58 @@ class CloseStrategy(val cfg: Config, val timeSeries: TimeSeries, val buyTick: In
                     break
                 }
             }
-            chart.priceData = (initialTick..lastTickEnd).map {
-                val bar = series.getBar(it)
-                Candle(
-                    bar.endTime.toEpochSecond(),
-                    bar.openPrice.doubleValue(),
-                    bar.closePrice.doubleValue(),
-                    bar.maxPrice.doubleValue(),
-                    bar.minPrice.doubleValue())
-            }
-            chart.operations = operations
-            chart.priceIndicators = chartWriter.priceIndicators
-            chart.extraIndicators = chartWriter.extraIndicators
-            chart.fill()
 
             if (sellPrice == 0.0) {
-                profitLabel.text = "no sell"
-                println("no sell")
+                if (!multipleTimes) {
+                    profitLabel.text = "no sell"
+                    println("no sell")
+                }
                 return
             }
+
+            // calculate results for strategy
             fun calcPercent(p: Double) = (p / initialPrice) * 100.0
             val profit = sellPrice - initialPrice
             val maxConst = maxPrice - initialPrice
             val minConst = minPrice - initialPrice
-            profitLabel.text =
-                ("Profit: $%.3f (%.1f%s)\n" +
-                    "Max: $%.3f (%.1f%s)\n" +
-                    "Min: $%.3f (%.1f%s)\n" +
-                    "Holding: $%.3f (%.3f%s)") // TODO: para holding tendria que esperar a que termine.
-                    .format(
-                        profit, calcPercent(profit), "%",
-                        maxConst, calcPercent(maxConst), "%",
-                        minConst, calcPercent(minConst), "%",
-                        999999.0, 0.0, "%"
-                    )
             profitSum += calcPercent(profit)
-            println("profit: ${calcPercent(profit)}%. sum: $profitSum%")
-            if (profit < 0.0) {
-                profitLabel.textFill = Color.RED
-            } else {
-                profitLabel.textFill = Color.GREEN
+
+            // calculate results for random
+            val profitRandom = series.getBar(lastTickEnd).closePrice.doubleValue() - initialPrice
+            randomSum += calcPercent(profitRandom)
+
+            // display stuff if necessary
+            if (!multipleTimes && chartWriter != null) {
+                chart.priceData = (initialTick..lastTickEnd).map {
+                    val bar = series.getBar(it)
+                    Candle(
+                        bar.endTime.toEpochSecond(),
+                        bar.openPrice.doubleValue(),
+                        bar.closePrice.doubleValue(),
+                        bar.maxPrice.doubleValue(),
+                        bar.minPrice.doubleValue())
+                }
+                chart.operations = operations
+                chart.priceIndicators = chartWriter.priceIndicators
+                chart.extraIndicators = chartWriter.extraIndicators
+                chart.fill()
+                profitLabel.text =
+                    ("Profit: $%.3f (%.1f%s)\n" +
+                        "Max: $%.3f (%.1f%s)\n" +
+                        "Min: $%.3f (%.1f%s)\n" +
+                        "Holding: $%.3f (%.3f%s)") // TODO: para holding tendria que esperar a que termine.
+                        .format(
+                            profit, calcPercent(profit), "%",
+                            maxConst, calcPercent(maxConst), "%",
+                            minConst, calcPercent(minConst), "%",
+                            999999.0, 0.0, "%"
+                        )
+                println("profit: ${calcPercent(profit)}%. sum: $profitSum%")
+                if (profit < 0.0) {
+                    profitLabel.textFill = Color.RED
+                } else {
+                    profitLabel.textFill = Color.GREEN
+                }
             }
         }
 
@@ -163,7 +194,19 @@ class CloseStrategy(val cfg: Config, val timeSeries: TimeSeries, val buyTick: In
                 setOnAction {
                     val cfg = gson.fromJson<Config>(jsonTextArea.text, Config::class.java)
                     cfg.saveTo("CloseStrategyTestConfig.json")
-                    execute(cfg)
+                    if (cfg.times > 1) {
+                        profitSum = 0.0
+                        randomSum = 0.0
+                        repeat(cfg.times) {
+                            execute(cfg)
+                        }
+                        val times = cfg.times
+                        println(" ")
+                        println("trading: $profitSum ($times times). profit per trade avg: ${profitSum / times}")
+                        println("random: $randomSum ($times times). profit per trade avg: ${randomSum / times}")
+                    } else {
+                        execute(cfg)
+                    }
                 }
             }
             val retryButton = Button("Retry").apply {
