1fe97964bb411022900c42a513e78587b3ef98bd
lleps <leandro.barbero122@gmail.com>
2019-07-25 19:53:22 -0300

    predictionserver.py using web sockets

--- model/predictionserver.py
+++ model/predictionserver.py
@@ -1,27 +1,71 @@
-import numpy
-import tensorflow as tf
-import pandas as pd
-import sys
 import asyncio
-import websockets
-from pandas import read_csv
-from pandas import DataFrame
-from pandas import concat
 from timeit import default_timer as timer
 
+import numpy
+import tensorflow as tf
+import websockets
+from tensorflow.python.platform import gfile
 
-# the point of this file is to allow to make predictions
-# using any keras model.
-
+# global state
 model = None
-arr = None
+sess = None
+in_tensor = None
+out_tensor = None
+
+
+# used to convert from keras to tensorflow model
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    from tensorflow.python.framework.graph_util import convert_variables_to_constants
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        # Graph -> GraphDef ProtoBuf
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = convert_variables_to_constants(session, input_graph_def,
+                                                      output_names, freeze_var_names)
+        return frozen_graph
+
 
 def process(msg, content):
+    # load:/media/lleps/Compartido/Dev/tradexchange/data/models/[train]fafafa-open.h5
+    # predict:0.9,0.2|0.4,0.2|0.01,0.19
     global model
     global arr
+    global sess
+    global in_tensor
+    global out_tensor
+
     if msg == "load":
+        # load keras model
+        tf.keras.backend.set_learning_phase(0)
         model = tf.keras.models.load_model(content)
+        in_tensor_name = model.input[0].name
+        out_tensor_name = model.output[0].name
+
+        # save as tensorflow model
+        frozen_graph = freeze_session(tf.keras.backend.get_session(),
+                                      output_names=[out.op.name for out in model.outputs])
+        tf.train.write_graph(frozen_graph, "log", "tmp_tf_model.pbtxt", as_text=True)
+
+        # load it
+        sess = tf.Session()
+        f = gfile.FastGFile("./log/tmp_tf_model.pbtxt", 'rb')
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        f.close()
+        sess.graph.as_default()
+        tf.import_graph_def(graph_def)
+
+        # get tensors
+        in_tensor = sess.graph.get_tensor_by_name('import/' + in_tensor_name)
+        out_tensor = sess.graph.get_tensor_by_name('import/' + out_tensor_name)
         return "ok"
+
     elif msg == "predict":
         if model is None:
             return "model not loaded"
@@ -35,35 +79,29 @@ def process(msg, content):
             features_per_line = len(lines[0].split(','))
             arr = numpy.zeros((1, timesteps, features_per_line), dtype=float)
 
-        for x in range(1):
-            i = 0
-            for l in lines:
-                features = l.split(',')
-                j = 0
-                for f in features:
-                    arr[x, i, j] = float(f)
-                    j += 1
-                i += 1
+        for i in range(len(lines)):
+            features = lines[i].split(',')
+            for j in range(len(features)):
+                arr[0, i, j] = float(features[j])
 
-        return str(model.predict(arr)[0])
+        result = sess.run(out_tensor, {in_tensor: arr})
+        return str(result[0])
 
 
-#load:/media/lleps/Compartido/Dev/tradexchange/data/models/[train]fafafa-open.h5
-#predict:0.9,0.2|0.4,0.2|0.01,0.19
+async def handle_request(socket, _):
+    while True:
+        # receive and parse msg
+        msg = await socket.recv()
+        msgtype, content = msg.split(':', 1)
+        start = timer()
+        result = process(msgtype, content)
+        took = timer() - start
+        print("response:", result, "took:", took, "sec")
+        await socket.send(result)
+
+
 if __name__ == '__main__':
-    while 1:
-        try:
-            fulldata = sys.stdin.readline().rstrip()
-            if fulldata == "" or fulldata == "bye":
-                print("gg wp")
-                break
-
-            msg, content = fulldata.split(':', 1)
-            start = timer()
-            result = process(msg, content)
-            result = process(msg, content)
-            result = process(msg, content)
-            took = timer() - start
-            print("response:", result, "(in ", took, "seconds)")
-        except KeyboardInterrupt:
-            break
+    process("load", "/media/lleps/Compartido/Dev/tradexchange/data/models/[train]fafafa-open.h5")
+    #start_server = websockets.serve(handle_request, "localhost", 8765)
+    #asyncio.get_event_loop().run_until_complete(start_server)
+    #asyncio.get_event_loop().run_forever()
