632f75378da743ff27a79fd89caa6b14b1375cb9
lleps <leandro.barbero122@gmail.com>
2019-08-07 12:31:51 -0300

    Implement training in predictionserver.py * to reduce startup time of the training

--- model/predictionserver.py
+++ model/predictionserver.py
@@ -4,19 +4,62 @@ import numpy
 import tensorflow as tf
 import json
 import websockets
+from pandas import DataFrame
+from pandas import concat
+from timeit import default_timer as timer
 
 # global state, for both buy and sell models
 sess_buy, sess_sell = None, None
 in_tensor_buy, in_tensor_sell = None, None
 out_tensor_buy, out_tensor_sell = None, None
+train_sess, train_model, train_X, train_y = None, None, None, None
 
 
-## problema: al leer el grafo, no tiene operaciones.
-## se puede isolar.
-## capaz que no tiene el formato correcto.. o no se.
-## 1. verificar que se exporte el .pb, chequear que no este vacio y que sea ese.
-## 2. isolar el cargado, para descartar problema del grafo.
-## 3. una vez arreglado, unir al codigo principal
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    from tf_graph_util import convert_variables_to_constants
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        # Graph -> GraphDef ProtoBuf
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = convert_variables_to_constants(session, input_graph_def,
+                                                      output_names, freeze_var_names)
+        return frozen_graph
+
+
+def join_past_rows_features(data, past_rows=1, future_rows=1, dropnan=True):
+    """
+    For each row, join the features since past_rows up to future_rows, so
+    it may be reshaped for a recurrent NN input.
+    """
+
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(past_rows, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, future_rows):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+
+    # put it all together
+    agg = concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
 
 
 def load_model(path):
@@ -65,6 +108,73 @@ def process(msg, content):
     global sess_buy, sess_sell
     global in_tensor_buy, in_tensor_sell
     global out_tensor_buy, out_tensor_sell
+    global train_sess, train_model, train_X, train_y
+
+    # train
+    if msg == "train_init": # to prepare the data and build the model architecture, based on the given timesteps
+        tf.keras.backend.clear_session()
+
+        params = content.split(",", 2)
+        csv_path = params[0]
+        timesteps = int(params[1])
+
+        # load csv into x and y
+        print("loading txt...")
+        dataset = numpy.loadtxt(csv_path, delimiter=",")
+        x = dataset[:, 1:-1] # remove price column (the first) and output column (the last)
+        feature_count = x[0].size
+        y = dataset[:, -1:]
+
+        # on each row, include the features from the previous (timestep - 1) rows.
+        # so, will end having feature_count*timesteps features per row.
+        x = join_past_rows_features(x, past_rows=timesteps - 1).values  # from t(timesteps-1) to t(0)
+        x = x.reshape((x.shape[0], timesteps, feature_count))
+        y = y[timesteps - 1:] # remove first _timesteps_ entries, to keep sample count in sync with x
+        print("data preprocessing done. shape:", x.shape)
+
+        # build the model
+        train_sess = tf.Session()
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.GRU(32, input_shape=(timesteps, feature_count)))
+        model.add(tf.keras.layers.Dropout(0.2))
+        model.add(tf.keras.layers.Dense(32, activation='relu'))
+        model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        print("model compiled.")
+
+        # save to global state
+        train_model = model
+        train_X = x
+        train_y = y
+        return "ok"
+
+    elif msg == "train_fit":
+        if train_model is None:
+            return "error: model not initialized"
+
+        params = content.split(",", 2)
+        epochs = int(params[0])
+        batch_size = int(params[1])
+        start = timer()
+        train_model.fit(train_X, train_y, epochs=epochs, batch_size=batch_size, verbose=2)
+        scores = train_model.evaluate(train_X, train_y)
+        time = timer() - start
+        return "ok: loss: %f, acc: %f, time: %f)" % (scores[0], scores[1], time)
+
+    elif msg == "train_save": # :path
+        if train_model is None:
+            return "error: model not initialized"
+
+        pb_path = content
+        tensor_in_name = train_model.input.name
+        tensor_out_name = train_model.output.name
+        with open(pb_path + "_meta.json", 'w') as f:
+            json.dump({'input_name': tensor_in_name, 'output_name': tensor_out_name}, f)
+
+        frozen_graph = freeze_session(tf.keras.backend.get_session(),
+                                      output_names=[out.op.name for out in train_model.outputs])
+        tf.train.write_graph(frozen_graph, ".", pb_path, as_text=False)
+        return "ok"
 
     # load
     if msg == "buy_load":
@@ -84,13 +194,13 @@ def process(msg, content):
     # predict
     elif msg == "buy_predict":
         if sess_buy is None:
-            return "sess_buy not initialized"
+            return "error: sess_buy not initialized"
 
         return str(do_prediction(content, sess_buy, in_tensor_buy, out_tensor_buy))
 
     elif msg == "sell_predict":
         if sess_sell is None:
-            return "sess_sell not initialized"
+            return "error: sess_sell not initialized"
 
         return str(do_prediction(content, sess_sell, in_tensor_sell, out_tensor_sell))
 
@@ -106,11 +216,19 @@ async def handle_request(socket, _):
 
 
 if __name__ == '__main__':
+    ## test loading and predicting
     # print(process("buy_load", "/media/lleps/Compartido/Dev/tradexchange/data/models/[train]fafafa-open.pb"))
     # print(process("sell_load", "/media/lleps/Compartido/Dev/tradexchange/data/models/[train]fafafa-open.pb"))
     # print(process("buy_predict", "0.9,0.2|0.4,0.2|0.01,0.19"))
     # print(process("sell_predict", "0.9,0.2|0.4,0.2|0.01,0.19"))
-    # #print(result)
+
+    ## test training and saving
+    # print(process("train_init", "/media/lleps/Compartido/Dev/tradexchange/data/trainings/[train]besttrainever.csv,4"))
+    # print(process("train_fit", "10,32"))
+    # print(process("train_save", "/home/lleps/some.pb"))
+    # print(process("train_init", "/media/lleps/Compartido/Dev/tradexchange/data/trainings/[train]besttrainever.csv,8"))
+    # print(process("train_fit", "10,32"))
+    # print(process("train_save", "some2.pb"))
 
     if len(sys.argv) != 3:
         print(sys.argv[0], "<host> <port>")
